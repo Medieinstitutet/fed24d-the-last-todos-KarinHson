39ee7c9f140187ed5dbe29343994d4006e19c2c5
Karin Henriksson <karin.henriksson@medieinstitutet.se>
2025-06-02 11:20:14 +0200

    ✅ fix: add id to Todo model

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -1,9 +1,11 @@
 export class Todo {
+    id: number;
     content: string;
     created_at: Date;
     isDone: boolean;
 
     constructor (content: string, created_at: Date, isDone: boolean) {
+        this.id = Date.now();
         this.content = content;
         this.created_at = created_at;
         this.isDone = isDone;
